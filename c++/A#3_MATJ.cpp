da2a61123f94612f2498f4ae5e9245a12875deae
Maotrex <maurigol10r@gmail.com>
2024-04-07 02:42:19 -0700

    Actualizar tarea 3

--- c++/A#3_MATJ.cpp
+++ c++/A#3_MATJ.cpp
@@ -1,6 +1,7 @@
 #include <iostream>
 #include <stdlib.h>
 #include <string>
+
 using namespace std;
 
 class Cliente
@@ -8,15 +9,15 @@ class Cliente
 
 private: // atributos o variables class "cliente"
     string customerName;
-    string Address;
-    string Email;
-    string CreditcardInfo;
-    float AccountBalance;
+    string address;
+    string email;
+    string creditcardInfo;
+    float accountBalance;
 
 public:        // metodos
     Cliente(); // Constructor
     void signup();
-    void login();
+    void showInfo();
 };
 
 // Constructor, nos sirve para inicializar los atributos
@@ -26,53 +27,70 @@ Cliente::Cliente()
 
 void Cliente::signup()
 {
-    cout << "Ingrese su nombre:\n ";
-    cin >> customerName;
-    cout << "Ingrese su direccion:\n ";
-    cin >> Address;
-    cout << "Ingrese su correo electronico:\n ";
-    cin >> Email;
-    cout << "Ingrese su información de tarjeta de crédito o débito:\n ";
-    cin >> CreditcardInfo;
-    cout << "Ingrese su saldo actual: \n";
-    cin >> AccountBalance;
+    cout << "**************************** \n";
+    cout << "******** Registro ********** \n";
+    cout << "Ingrese su nombre: ";
+    // elimina todos los espacios que puedan ser acarreados de la impresion anterior.
+    cin >> ws;
+    getline(cin, customerName);
+
+    cout << "Ingrese su direccion: ";
+    getline(cin, address);
+
+    cout << "Ingrese su correo electronico: ";
+    getline(cin, email);
+
+    cout << "Ingrese su información de tarjeta de crédito o débito [credito/debito]: ";
+    getline(cin, creditcardInfo);
+
+    cout << "Ingrese su saldo actual: ";
+    cin >> accountBalance;
 }
 
-void Cliente::login()
+void Cliente::showInfo()
 {
+    string respuesta;
 
-    cout << "Bienvenido a su cuenta, " << customerName << ". La informacion de su cuenta es:\n " << endl;
-    cout << "Nombre: \n"
-         << customerName << endl;
-    cout << "Direccion: \n"
-         << Address << endl;
-    cout << "Email: \n"
-         << Email << endl;
-    cout << "Informacion de tarjeta: \n"
-         << CreditcardInfo << endl;
-    cout << "Saldo actual: \n"
-         << AccountBalance << endl;
+    cout << "**************************** \n";
+    cout << "******* Bienvenido ********* \n";
+    cout << "Desea consultar su información? [si/no]: ";
+    cin >> respuesta;
+
+    if (respuesta == "si" || respuesta == "yes")
+    {
+        cout << "**************************** \n";
+        cout << "****** Buenas tardes ******* \n";
+        cout << "Bienvenido a su cuenta, " << customerName << ". La informacion de su cuenta es:\n " << endl;
+        cout << "Nombre: \n"
+             << customerName << endl;
+        cout << "Direccion: \n"
+             << address << endl;
+        cout << "Email: \n"
+             << email << endl;
+        cout << "Informacion de tarjeta: \n"
+             << creditcardInfo << endl;
+        cout << "Saldo actual: \n"
+             << accountBalance << endl;
+    }
+    else
+    {
+        cout << "Gracias por su visita." << endl;
+    }
 }
 
 int main()
 {
+    Cliente cliente1;
+    cliente1.signup();
+    cliente1.showInfo();
 
-    Cliente c1;
+    Cliente cliente2;
+    cliente2.signup();
+    cliente2.showInfo();
 
-    c1.signup();
-
-    string respuesta;
-    cout << "Bienvenido, desea consultar su información?\n";
-    cin >> respuesta;
-    if (respuesta == "si" || respuesta == "yes")
-    {
-        c1.login();
-    }
-    else
-    {
-        cout << "Gracias por su visita.\n"
-             << endl;
-    }
+    Cliente cliente3;
+    cliente3.signup();
+    cliente3.showInfo();
 
     return 0;
 }
